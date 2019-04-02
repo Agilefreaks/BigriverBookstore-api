ebf41b0b2b415ac0774dd8aa735f59a696ce80a5
George.Bancila <george.bancila93@gmail.com>
2019-04-02 15:48:30 +0300

    Retrieve a list of books

---
+++ BigriverBookstore_api/BigriverBookstore_api/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JsonApiDotNetCore.Controllers;
+using JsonApiDotNetCore.Services;
+using BigriverBookstore_api.Models;
+
+namespace BigriverBookstore_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : JsonApiController<Book>
+    {
+        public BooksController(IJsonApiContext jsonApiContext, IResourceService<Book> resourceService) : base(jsonApiContext, resourceService) { }
+    }
+}
---
+++ BigriverBookstore_api/BigriverBookstore_api/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JsonApiDotNetCore.Models;
+
+namespace BigriverBookstore_api.Models
+{
+    public class Book : Identifiable
+    {
+        [Attr("title")]
+        public string Title { get; set; }
+
+        [Attr("date_published")]
+        public DateTime Date_Published { get; set; }
+
+        [Attr("isbn")]
+        public string ISBN { get; set; }
+    }
+}
