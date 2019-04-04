4373c7e3bcf7ca9d7c46ae7c10bd62241bc37afc
George.Bancila <george.bancila93@gmail.com>
2019-04-04 18:24:32 +0300

    Link author to book

--- BigriverBookstore_api/BigriverBookstore_api/Data/RepositoryWrapper.cs
+++ BigriverBookstore_api/BigriverBookstore_api/Data/RepositoryWrapper.cs
@@ -50,85 +50,47 @@ namespace BigriverBookstore_api.Data
 
         private void AddFakeBookData()
         {
-            this._book.Add(new Book
+            for (int i = 0; i < 5; i++)
             {
-                Id = 1,
-                Title = "My Book1",
-                ISBN = "978-3-16-148410-1",
-                Date_Published = new DateTime(2019, 04, 01)
-            });
-            this._book.Add(new Book
-            {
-                Id = 2,
-                Title = "My Book2",
-                ISBN = "978-3-16-148410-2",
-                Date_Published = new DateTime(2019, 04, 01)
-            });
-            this._book.Add(new Book
-            {
-                Id = 3,
-                Title = "My Book3",
-                ISBN = "978-3-16-148410-3",
-                Date_Published = new DateTime(2019, 04, 01)
-            });
-            this._book.Add(new Book
-            {
-                Id = 4,
-                Title = "My Book4",
-                ISBN = "978-3-16-148410-4",
-                Date_Published = new DateTime(2019, 04, 01)
-            });
-            this._book.Add(new Book
-            {
-                Id = 5,
-                Title = "My Book5",
-                ISBN = "978-3-16-148410-5",
-                Date_Published = new DateTime(2019, 04, 01)
-            });
+                var book = this.GetBook(i);
+                book.Author = GetAuthor(i);
+                _book.Add(book);
+            }
         }
 
         private void AddFakeAuthorData()
         {
-            this._author.Add(new Author
+            for (int i = 0; i < 5; i++)
             {
-                Id = 1,
-                FirstName = "FN1",
-                LastName = "LN1",
-                DateOfBirth = DateTime.Now,
-                Nationality = "Romanian"
-            });
-            this._author.Add(new Author
+                var author = this.GetAuthor(i);
+                author.Book = GetBook(i);
+                _author.Add(author);
+            }
+        }
+
+        private Book GetBook(int id)
+        {
+            return new Book
             {
-                Id = 2,
-                FirstName = "FN2",
-                LastName = "LN2",
-                DateOfBirth = DateTime.Now,
-                Nationality = "Romanian"
-            });
-            this._author.Add(new Author
+                Id = id,
+                Title = "My Book" + id.ToString(),
+                ISBN = "978-3-16-148410-" + id.ToString(),
+                Date_Published = new DateTime(2019, 04, 01),
+                AuthorId = id
+            };
+        }
+
+        private Author GetAuthor(int id)
+        {
+            return new Author
             {
-                Id = 3,
-                FirstName = "FN3",
-                LastName = "LN3",
+                Id = id,
+                FirstName = "FN" + id.ToString(),
+                LastName = "LN" + id.ToString(),
                 DateOfBirth = DateTime.Now,
-                Nationality = "Romanian"
-            });
-            this._author.Add(new Author
-            {
-                Id = 4,
-                FirstName = "FN4",
-                LastName = "LN4",
-                DateOfBirth = DateTime.Now,
-                Nationality = "Romanian"
-            });
-            this._author.Add(new Author
-            {
-                Id = 5,
-                FirstName = "FN5",
-                LastName = "LN5",
-                DateOfBirth = DateTime.Now,
-                Nationality = "Romanian"
-            });
+                Nationality = "Romanian",
+                BookId = id
+            };
         }
     }
 }
