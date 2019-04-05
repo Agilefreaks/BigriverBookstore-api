d66e1dcbd5e0a985dfa67c642aff016ea4fa2dcb
George.Bancila <george.bancila93@gmail.com>
2019-04-05 15:57:19 +0300

    define photos

---
+++ BigriverBookstore_api/BigriverBookstore_api/Data/Entities/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BigriverBookstore_api.Data.Entities
+{
+    public class Photo : BaseEntity
+    {
+        public string URL { get; set; }
+
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
---
+++ BigriverBookstore_api/BigriverBookstore_api/Resources/Photo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using JsonApiDotNetCore.Models;
+
+namespace BigriverBookstore_api.Resources
+{
+    public class Photo : Identifiable
+    {
+        [Attr("url")]
+        public string URL { get; set; }
+
+        [HasOne("book")]
+        public virtual Book Book { get; set; }
+    }
+}
