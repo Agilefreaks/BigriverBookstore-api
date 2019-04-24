50e6d140b9d6cd280cd4cb411b0e8ef661f8899e
marek231 <pruteanupopescumarek@gmail.com>
2019-04-24 14:38:59 +0300

    Added response content test for author.

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from api import create_app
+from api.mock_db import db
 
 # Big TODO
 
@@ -19,13 +20,18 @@ class TestCase(unittest.TestCase):
         response = self.app.get('/books/')
         self.assertTrue(response.mimetype == 'application/vnd.api+json')
 
-    def test_all_authors_api_response(self):
+    def test_all_authors_api_successful(self):
         response = self.app.get('/authors/')
         self.assertEqual(response.status_code, 200)
 
     def test_all_authors_api_mimetype(self):
         response = self.app.get('/authors/')
         self.assertTrue(response.mimetype == 'application/vnd.api+json')
+
+    def test_all_authors_api_content(self):
+        response = self.app.get('/authors/')
+        self.assertTrue(response.json['data'][1]['attributes']
+                        ['first_name'] == db['authors'][1].first_name)
 
 
 if __name__ == '__main__':
