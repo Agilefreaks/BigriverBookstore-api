5b6974291fbf819ebace843fda90881b97a6485c
Muresan Beniamin <muresan.beniamin@hotmail.com>
2020-05-26 10:14:40 +0300

    Add show action and spec

--- app/controllers/v1/books_controller.rb
+++ app/controllers/v1/books_controller.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 class V1::BooksController < ApplicationController
   def index
     @pagy, books = pagy(Book.all)
     render json: BookSerializer.new(books).serialized_json
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: BookSerializer.new(book).serialized_json
   end
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,22 +1,43 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe V1::BooksController do
-  let! (:book1) { Book.create(title: 'Moby Dick') }
+  let!(:book1) { Book.create(title: 'Moby Dick') }
 
-  describe "GET #index" do
+  describe 'GET #index' do
     before do
       get :index
     end
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
-    it "renders expected books attributes" do
+    it 'renders expected books attributes' do
       json_response_data = JSON.parse(response.body)['data']
-      expect(json_response_data.first['attributes'].keys).to match_array(['title',
-                                                                          'isbn',
-                                                                          'author',
-                                                                          'image_url', 
-                                                                          'release_year'])
+      expect(json_response_data.length).to eq 1
+      expect(json_response_data.first['attributes'].keys).to match_array(%w(title
+                                                                            isbn
+                                                                            author
+                                                                            image_url
+                                                                            release_year
+                                                                            description))
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      get :show, params: { id: book1.id }
+    end
+
+    it 'returns the book' do
+      json_response_data = JSON.parse(response.body)['data']
+      expect(response).to have_http_status(:success)
+      expect(json_response_data['attributes']).to eq({ 'title' => book1.title,
+                                                       'description' => book1.description,
+                                                       'author' => book1.author,
+                                                       'isbn' => book1.isbn,
+                                                       'release_year' => book1.release_year,
+                                                       'image_url' => book1.image_url })
     end
   end
 end
