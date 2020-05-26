053f55fd76abd487f8d2a42f44f7dbda4d787fc3
Muresan Beniamin <muresan.beniamin@hotmail.com>
2020-05-26 10:15:08 +0300

    Add description in serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class BookSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :title, :author, :isbn, :release_year, :image_url
+  attributes :title, :author, :isbn, :release_year, :image_url, :description
 end
